1175187ed8dfca499729d4abe1e4b7c0d7c21028
Charles Nii Adotey Allotey <charlesallotey1995@gmail.com>
2023-03-27 09:10:10 +0000

    add lesson 9

---
+++ src/lesson9.cjs
@@ -0,0 +1,8 @@
+ export default function sum(a,b) {
+    return a + b    
+}
+
+
+export function multiply (a, b) {
+    return a * b
+}
---
+++ src/lesson9b.cjs
@@ -0,0 +1,7 @@
+//import sum, {multiply } from "/src/lesson9"
+
+const {multiply} = require('./lesson9')
+
+
+//console.log(sum(1,2))
+console.log(multiply(1,2))
